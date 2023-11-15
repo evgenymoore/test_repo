cf861ab8e4759119b3a11603b2edc928327074e1
Евгений Мурашов <murashov@inventa.su>
2023-11-15 16:10:47 +0300

    Завершение работы над canMakeArithmeticProgression()

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stack>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
@@ -24,7 +25,18 @@ public:
         return s;
     }
 
-    bool canMakeArithmeticProgression(vector<int>& arr) {}
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](const auto& rhs, const auto& lhs) {
+            return rhs < lhs;
+        });
+        int diff = arr[1] - arr[0];
+        for (int i = 1; i != arr.size(); ++i) {
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 
 int main() {
@@ -40,5 +52,9 @@ int main() {
     cout << solution.removeDuplicates("abcdef") << endl;
     cout << solution.removeDuplicates("abcadefa") << endl;
     cout << solution.removeDuplicates("aababaab") << endl;
+
+    vector<int> array {5, 3, 1};
+    assert(solution.canMakeArithmeticProgression(array));
+
     return 0;
 }

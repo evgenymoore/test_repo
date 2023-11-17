081bf9e5484cbd0c6bb9cd2c0b9ba26fb7aea03d
Евгений Мурашов <murashov@inventa.su>
2023-11-17 12:07:52 +0300

    Исправление критического бага версии v0.1

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ public:
         });
         int diff = arr[1] - arr[0];
         for (int i = 1; i != arr.size(); ++i) {
-            if (arr[i] - arr[i - 1] != diff) {
+            if (arr[i] - arr[i - 1] == diff) {
                 return false;
             }
         }
